7380df1a3a3eedcf79d239f2321e488bc4b60f37
jtomatoo <jtoamtoo@gmail.com>
2019-02-07 13:01:36 +0900

    vol6 item 35

---
+++ src/main/java/domain/Ensemble.java
@@ -0,0 +1,28 @@
+package domain;
+
+public enum Ensemble {
+
+	SOLO(1),
+	DUET(2),
+	TRIO(3),
+	QUARTET(4),
+	QUINTET(5),
+	SEXTET(6),
+	SEPTET(7),
+	OCTET(8),
+	DOUBLE_QUARTET(8),
+	NONET(9),
+	DECTET(10),
+	TRIPLE_QUARTET(12);
+	
+	private final int numberOfMusicians;
+
+	private Ensemble(int numberOfMusicians) {
+		this.numberOfMusicians = numberOfMusicians;
+	}
+	
+	public int numberOfMusicians() {
+		return numberOfMusicians;
+	}
+	
+}
